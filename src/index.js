23d4fc2f5a0333eb5f6bc52438d07eaad73a09de
Iryna Chystabayeva <Iryna.Chystabayeva@itechart-group.com>
2020-10-13 17:02:49 +0300

    feat: implement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let newArr = [];
+  if (matrix != undefined) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 != 0) {
+        matrix[i].reverse();
+      }
+    }
+    newArr = matrix.flat();
+  }
+
+  return newArr;
 }
